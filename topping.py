a5e8b393f0a4254c6e9d28502da99e16cc2c5987
jorobertmac <160981126+jorobertmac@users.noreply.github.com>
2025-02-28 18:49:10 -0700

    Add function to update a topping currently on the list with

--- topping.py
+++ topping.py
@@ -15,3 +15,11 @@ class ToppingList:
     topping = topping.strip().lower()
     if topping in self.list:
       self.list.remove(topping)
+
+  def update(self, old_topping: str, new_topping: str):
+    old_topping = old_topping.strip().lower()
+    new_topping = new_topping.strip().lower()
+    if old_topping not in self.list or new_topping in self.list:
+      return
+    index = self.list.index(old_topping)
+    self.list[index] = new_topping
